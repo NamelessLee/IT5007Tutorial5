57f61eda9286e10764bd2a0ec00a412e8bf7c5d8
Xinyue <296725320@qq.com>
2021-10-25 00:22:36 +0800

    Init and add Add function

---
+++ scripts/init.mongo.js
@@ -0,0 +1,31 @@
+/*
+ * Run using the mongo shell. For remote databases, ensure that the
+ * connection string is supplied in the command line. For example:
+ * localhost:
+ *   mongo issuetracker scripts/init.mongo.js
+ * Atlas:
+ *   mongo mongodb+srv://user:pwd@xxx.mongodb.net/issuetracker scripts/init.mongo.js
+ * MLab:
+ *   mongo mongodb://user:pwd@xxx.mlab.com:33533/issuetracker scripts/init.mongo.js
+ */
+
+db.issues.remove({});
+
+const issuesDB = [
+  {
+    id: 1, 
+    serialNumber: '000001',
+    name: 'LXY',
+    phoneNumber: '12345678',
+    created: new Date('2019-01-15'),
+  }
+];
+
+//db.issues.insertMany(issuesDB);
+const count = db.issues.count();
+print('Inserted', count, 'issues');
+
+db.counters.remove({ _id: 'issues' });
+db.counters.insert({ _id: 'issues', current: count });
+
+db.issues.createIndex({ id: 1 }, { unique: true });
---
+++ src/App.jsx
@@ -0,0 +1,163 @@
+const dateRegex = new RegExp('^\\d\\d\\d\\d-\\d\\d-\\d\\d');
+
+function jsonDateReviver(key, value) {
+  if (dateRegex.test(value)) return new Date(value);
+  return value;
+}
+
+class IssueFilter extends React.Component {
+  render() {
+    return (
+      <div>This is a placeholder for the issue filter.</div>
+    );
+  }
+}
+
+function IssueRow(props) {
+  const issue = props.issue;
+  return (
+    <tr>
+      <td>{issue.id}</td>
+      <td>{issue.serialNumber}</td>
+      <td>{issue.name}</td>
+      <td>{issue.phoneNumber}</td>
+      <td>{issue.created.toDateString()}</td>
+    </tr>
+  );
+}
+
+function IssueTable(props) {
+  const issueRows = props.issues.map(issue =>
+    <IssueRow key={issue.id} issue={issue} />
+  );
+
+  return (
+    <table className="bordered-table">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Serial Number</th>
+          <th>Name</th>
+          <th>Phone Number</th>
+          <th>Created</th>
+        </tr>
+      </thead>
+      <tbody>
+        {issueRows}
+      </tbody>
+    </table>
+  );
+}
+
+class IssueAdd extends React.Component {
+  constructor() {
+    super();
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const form = document.forms.issueAdd;
+    const issue = {
+      name: form.name.value,
+      phoneNumber: form.phoneNumber.value
+    }
+    this.props.createIssue(issue);
+    form.name.value = ""; form.phoneNumber.value = "";
+  }
+
+  render() {
+    return (
+      <form name="issueAdd" onSubmit={this.handleSubmit}>
+        <label for="name" >Name:</label>
+        <input type="text" id="name" name="name" placeholder="Name" />
+        <br></br>
+        <label for="phoneNumber" >Phone Number:</label>
+        <input type="text" id="phoneNumber" name="phoneNumber" placeholder="Phone Number" />
+        <br></br>
+        <button>Add</button>
+      </form>
+    );
+  }
+}
+
+async function graphQLFetch(query, variables = {}) {
+  try {
+    const response = await fetch('/graphql', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json'},
+      body: JSON.stringify({ query, variables })
+    });
+    const body = await response.text();
+    const result = JSON.parse(body, jsonDateReviver);
+
+    if (result.errors) {
+      const error = result.errors[0];
+      if (error.extensions.code == 'BAD_USER_INPUT') {
+        const details = error.extensions.exception.errors.join('\n ');
+        alert(`${error.message}:\n ${details}`);
+      } else {
+        alert(`${error.extensions.code}: ${error.message}`);
+      }
+    }
+    return result.data;
+  } catch (e) {
+    alert(`Error in sending data to server: ${e.message}`);
+  }
+}
+
+class IssueList extends React.Component {
+  constructor() {
+    super();
+    this.state = { issues: [] };
+    this.createIssue = this.createIssue.bind(this);
+  }
+
+  componentDidMount() {
+    this.loadData();
+  }
+
+  async loadData() {
+    const query = `query {
+      issueList {
+        id serialNumber name phoneNumber
+        created 
+      }
+    }`;
+
+    const data = await graphQLFetch(query);
+    if (data) {
+      this.setState({ issues: data.issueList });
+    }
+  }
+
+  async createIssue(issue) {
+    const query = `mutation issueAdd($issue: IssueInputs!) {
+      issueAdd(issue: $issue) {
+        id
+      }
+    }`;
+
+    const data = await graphQLFetch(query, { issue });
+    if (data) {
+      this.loadData();
+    }
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <h1>California Hotel</h1>
+        <IssueFilter />
+        <hr />
+        <IssueTable issues={this.state.issues} />
+        <hr />
+        <IssueAdd createIssue={this.createIssue} />
+      </React.Fragment>
+    );
+  }
+}
+
+const element = <IssueList />;
+
+ReactDOM.render(element, document.getElementById('contents'));
